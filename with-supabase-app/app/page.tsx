c76766e55ed0bb8d677af6777ac097e28e28768b
junhodo <djh10209@gmail.com>
2025-02-17 09:27:18 +0900

    init project

---
+++ with-supabase-app/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <main className="flex-1 flex flex-col gap-6 px-2 items-center justify-center">
+        <h1 className="text-4xl font-bold">
+          AI Language Tutor for You
+        </h1>
+
+        <div className=" h-1/2">
+        </div>
+        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+      </main>
+    </>
+  );
+}
