a9d55284ebb9b4e9d8e4af3f2bf33a7f501470e7
junhodo <djh10209@gmail.com>
2025-02-25 03:38:35 +0900

    get domain from env

--- lib/be/utils/env.ts
+++ lib/be/utils/env.ts
@@ -1,9 +1,12 @@
 class EnvProvider {
     public nextPublicSupabaseUrl: string;
     public nextPublicSupabaseAnonKey: string;
+    public domain: string;
+    
     constructor() {
         this.nextPublicSupabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
         this.nextPublicSupabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+        this.domain = process.env.DOMAIN!;
     }
 }
 
--- lib/fe/superbase/signup.ts
+++ lib/fe/superbase/signup.ts
@@ -1,11 +1,12 @@
 import { createClient } from "@/lib/fe/superbase/client";
+import { env } from "@/lib/be/utils/env";
 
 export async function signUpWithGoogle() {
   const supabase = await createClient();
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${window.location.origin}/api/auth/callback/google`,
+      redirectTo: `https://${env.domain}/api/auth/callback/google`,
     },
   });
 
