0a0711cab592069e6125a56185d6ac550aabcfef
junhodo <djh10209@gmail.com>
2025-02-25 04:54:24 +0900

    update main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,19 @@
 export default function Home() {
   return (
     <>
-      <main className="flex-1 flex flex-col gap-6 px-2 items-center justify-center">
-        <h1 className="text-4xl font-bold">AI Language Tutor for You</h1>
+      <main className="flex-1 flex flex-col px-2 items-center justify-center">
+        <h1 className="text-5xl font-bold pb-2">
+          Chirpify, Learn English by <span style={{ color: "#0EF397" }}>Thinking</span>
+        </h1>
+        <h2 className="font-medium text-2xl mb-4 pb-4" style={{ color: "#7B7B7B" }}>
+          A new way to learn through exploration and active engagement.
+        </h2>
 
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        <h2 className="font-medium text-2xl mb-4">
+          Try your first lesson for free!
+        </h2>
+
+        <img src="/images/aru.png" alt="Aru"  style={{ width: "800px", height: "auto" }} />
       </main>
     </>
   );
