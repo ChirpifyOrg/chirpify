ed2161e077388080e2ef98b53cb8ccc600846408
junhodo <djh10209@gmail.com>
2025-02-26 02:16:01 +0900

    remove redundant codes

--- app/(auth)/components/header-auth.tsx
+++ app/(auth)/components/header-auth.tsx
@@ -1,8 +1,6 @@
-// import { signOutAction } from "@/lib/fe/superbase/sign-out";
 import Link from "next/link";
 import { Button } from "../../../components/ui/button";
 import { createClient } from "@/lib/be/superbase/server";
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { UserProfile } from "@/app/(user)/components/user_profile_avatar";
 
 export default async function AuthButton() {
@@ -15,17 +13,6 @@ export default async function AuthButton() {
   return user ? (
     <div className="flex items-center gap-4">
       <UserProfile /> 
-      {/* <Avatar>
-        <AvatarImage src={user.user_metadata.avatar_url} />
-        <AvatarFallback>
-          {user.user_metadata.name.charAt(0)}
-        </AvatarFallback>
-      </Avatar> */}
-      {/* <form action={signOutAction}>
-        <Button type="submit" variant={"outline"}>
-          Sign out
-        </Button>
-      </form> */}
     </div>
   ) : (
     <div className="flex gap-2">
