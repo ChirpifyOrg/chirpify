52f2a5496498146ee81d6f00d0bbb4e84f061ef0
bm150 <jbm20dev@gmail.com>
2025-04-23 18:43:47 +0900

    feat : 공통 Error <-> http protocol Error 로 변경하는 에러 처리 코드 추가.

--- app/api/chat/[roomId]/message/route.tsx
+++ app/api/chat/[roomId]/message/route.tsx
@@ -4,20 +4,23 @@ import { createClient } from '@/lib/be/superbase/server';
 import { ClientChatRequest } from '@/types/chat';
 import { createChatRequest } from '@/be/application/chat/Dtos';
 import { ChatUseCaseFactory } from '@/be/application/chat/ChatUseCaseFactory';
+import { toHttpError } from '@/lib/be/utils/error-http-mapper';
 
 export async function POST(request: NextRequest, { params }: { params: { roomId: string } }) {
    const { roomId } = params;
    const { message, nativeLanguage }: ClientChatRequest = await request.json();
-   const superbase = await createClient();
-   const { data, error } = await superbase.auth.getUser();
-   const isLoggedIn = !error && data?.user != null;
 
-   const userId = data?.user?.id;
-   if (!userId) {
-      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-   }
-   const isTrial = data?.user?.is_anonymous ? true : false;
    try {
+      const superbase = await createClient();
+      const { data, error } = await superbase.auth.getUser();
+      const isLoggedIn = !error && data?.user != null;
+
+      const userId = data?.user?.id;
+      if (!userId) {
+         return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+      }
+      const isTrial = data?.user?.is_anonymous ? true : false;
+
       const useCase = ChatUseCaseFactory.getInstance().getUseCase(isLoggedIn, isTrial);
       const response = await useCase.processChat(
          createChatRequest({ message, nativeLanguage, roomId }, userId, isTrial),
@@ -28,33 +31,35 @@ export async function POST(request: NextRequest, { params }: { params: { roomId:
          },
       });
    } catch (e) {
-      return NextResponse.json({ e: 'Error' }, { status: 500 });
+      const error = toHttpError(e);
+      return NextResponse.json(error.body, { status: error.status });
    }
 }
 
-export async function GET(req: NextRequest, { params }: { params: { roomId: string } }) {
-   const superbase = await createClient();
-   const { data, error } = await superbase.auth.getUser();
-   const isLoggedIn = !error && data?.user != null;
+export async function GET(req: NextRequest, { params: { roomId } }: { params: { roomId: string } }) {
+   try {
+      const superbase = await createClient();
+      const { data, error } = await superbase.auth.getUser();
+      const isLoggedIn = !error && data?.user != null;
+      const userId = data?.user?.id;
+      if (!userId) {
+         return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+      }
+      const isTrial = data?.user?.is_anonymous ? true : false;
+      const useCase = ChatUseCaseFactory.getInstance().getUseCase(isLoggedIn, isTrial);
+      const { searchParams } = req.nextUrl;
+      const startIndex = searchParams.get('startIndex') ? searchParams.get('startIndex')! : undefined;
+      const endIndex = searchParams.get('endIndex') ? searchParams.get('endIndex')! : undefined;
+      const limit = searchParams.get('limit') ? parseInt(searchParams.get('limit')!) : undefined;
 
-   const roomId = params.roomId;
-
-   const { searchParams } = req.nextUrl;
-   const startIndex = searchParams.get('startIndex') ? parseInt(searchParams.get('startIndex')!) : undefined;
-   const endIndex = searchParams.get('endIndex') ? parseInt(searchParams.get('endIndex')!) : undefined;
-   const limit = searchParams.get('limit') ? parseInt(searchParams.get('limit')!) : undefined;
-
-   // 여기에 서비스 호출 (예: ChatUseCase 등)
-   // const useCase = ChatUseCaseFactory.getInstance().getUseCase(isLoggedIn);
-   // const result = await useCase.getChatSimpleFormatHistory({ roomId, startIndex, endIndex, limit });
-
-   const dummyData = {
-      roomId,
-      startIndex,
-      endIndex,
-      limit,
-      messages: ['message 1', 'message 2'],
-   };
-
-   return NextResponse.json(dummyData);
+      const responseData = await useCase.getHistory({ roomId, startIndex, endIndex, limit });
+      return NextResponse.json(responseData, {
+         headers: {
+            'Content-Type': 'application/json',
+         },
+      });
+   } catch (e) {
+      const error = toHttpError(e);
+      return NextResponse.json(error.body, { status: error.status });
+   }
 }
