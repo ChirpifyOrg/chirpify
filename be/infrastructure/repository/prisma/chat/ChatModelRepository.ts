5f47fad69d192110564930a161f9c85d332222ff
bm150 <jbm20dev@gmail.com>
2025-04-28 23:26:44 +0900

    feat : streaming 지원으로 인한 채팅모델 구현체 코드 수정

--- be/infrastructure/repository/prisma/chat/ChatModelRepository.ts
+++ be/infrastructure/repository/prisma/chat/ChatModelRepository.ts
@@ -1,29 +1,44 @@
 import { ChatModel } from '@/be/domain/chat/ChatModel';
 import { ChatModelRepository } from '@/be/domain/chat/ChatModelRepository';
-import { chat_model } from '@/lib/generated/prisma';
+import { chat_model, chat_model_parameters } from '@/lib/generated/prisma';
 import { Prisma } from '@/lib/generated/prisma';
 import { BasePrismaRepository } from '../BasePrismaRepository';
+import { ChatModelParameter } from '@/be/domain/chat/ChatModelParameter';
 
 export class ChatModelRepositoryImpl extends BasePrismaRepository implements ChatModelRepository {
    // persona 이름으로 마지막 모델 정보 가져오기
-   async getLastModelInfo(name: string) {
+   async getLastModelInfo(name: string, isStreaming: boolean) {
       const prismaModel = await this.prisma.chat_model.findFirst({
-         where: { persona: name },
+         where: { persona: name, chat_model_parameters: { some: { is_streaming: isStreaming, is_active: true } } },
+         include: {
+            chat_model_parameters: true,
+         },
          orderBy: { created_at: 'desc' },
       });
       if (!prismaModel) return null;
       return ChatModelRepositoryImpl.toEntity(prismaModel);
    }
 
-   static toEntity(prismaModel: chat_model): ChatModel {
+   static toEntity(prismaModel: chat_model & { chat_model_parameters: chat_model_parameters[] }): ChatModel {
+      const chatModelParameters =
+         prismaModel?.chat_model_parameters.map(
+            param =>
+               new ChatModelParameter({
+                  id: param.id,
+                  prompt: param.prompt ?? undefined,
+                  defaultParam: param.default_param,
+                  isActive: param.is_active ?? true,
+                  isStremaing: param.is_streaming ?? false,
+                  createdAt: param.created_at,
+               }),
+         ) ?? [];
+
       return new ChatModel({
          id: prismaModel.id,
          persona: prismaModel.persona ?? undefined,
          description: prismaModel.description ?? undefined,
-         prompt: prismaModel.prompt ?? undefined,
-         isActive: prismaModel.is_active ?? undefined,
          createdAt: prismaModel.created_at,
-         defaultParam: prismaModel.default_param,
+         chatModelParameter: chatModelParameters, // ChatModelParameter 포함
          chatRooms: [], // 필요시 매핑
       });
    }
@@ -33,11 +48,17 @@ export class ChatModelRepositoryImpl extends BasePrismaRepository implements Cha
          id: entity.id,
          persona: entity.persona,
          description: entity.description,
-         prompt: entity.prompt,
-         is_active: entity.isActive,
          created_at: entity.createdAt,
-         default_param: entity.defaultParam,
-         // chat_rooms: [], // 관계형 입력은 조심
+         // chat_model_parameters: {
+         //    create: entity.chatModelParameter.map(param => ({
+         //       id: param.id,
+         //       prompt: param.prompt,
+         //       default_param: param.defaultParam,
+         //       is_active: param.isActive,
+         //       is_streaming: param.isStremaing,
+         //       created_at: param.createdAt,
+         //    })),
+         // },
       };
    }
 }
