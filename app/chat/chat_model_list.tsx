726b24856a8787b6bb95b22eef5d9b7b8b61714f
bm150 <jbm20dev@gmail.com>
2025-05-07 22:34:46 +0900

    feat : chat model list 표시 화면 및 채팅방 생성 후 이동 로직 추가

--- app/chat/chat_model_list.tsx
+++ app/chat/chat_model_list.tsx
@@ -1,8 +1,12 @@
 'use client';
-
 import { ChatModel } from '@/be/domain/chat/ChatModel';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { API_ENDPOINTS } from '@/lib/fe/api-endpoints';
 import Image from 'next/image';
+
+import { fetchWithTypedBody } from '../hooks/useFetchData';
+import { useRouter } from 'next/navigation';
+import { ChatRoom } from '@/be/domain/chat/ChatRoom';
 
 // Update the props type to accept an array of ChatModel
 type ChatModelListProps = {
@@ -10,31 +14,46 @@ type ChatModelListProps = {
    //    cardStyle?: CSSProperties;
 };
 export const ChatModelList = ({ data }: ChatModelListProps) => {
+   const router = useRouter();
+   const handleClick = async (modelId: string) => {
+      try {
+         const response = await fetchWithTypedBody<{ modelId: string }, ChatRoom>(API_ENDPOINTS.getOrCreateChatRoom(), {
+            method: 'POST',
+            body: { modelId },
+         });
+         router.push(`/chat/${response.id}`);
+      } catch (error) {
+         console.error('Error fetching data:', error);
+      }
+   };
+
    return (
       <>
-         {data?.map(({ persona, desc, image }) => (
-            <Card
-            //  style={cardStyle ?? {}}
-            >
-               <CardHeader>
-                  <CardTitle>{persona}</CardTitle>
-                  <CardDescription>{desc}</CardDescription>
-               </CardHeader>
-               <CardContent>
-                  <Image
-                     src={`/images/${persona}_calm.webp`}
-                     alt={persona ?? 'persona'}
-                     fill
-                     className="transition-all duration-300 object-cover"
-                     priority
-                  />
-                  <p>{image}</p>
-               </CardContent>
-               <CardFooter>
-                  <p>Card Footer</p>
-               </CardFooter>
-            </Card>
-         ))}
+         {data?.map(({ persona, id, description }) => {
+            return (
+               <div key={id} onClick={() => handleClick(id)} className="w-full cursor-pointer">
+                  <Card>
+                     <CardHeader>
+                        <CardTitle>
+                           <div className="w-full relative aspect-[3/2]">
+                              <Image
+                                 src={`/images/${persona}_joy.webp`}
+                                 alt={persona ?? 'persona'}
+                                 fill
+                                 sizes="100vw"
+                                 className="object-contain transition-all duration-300"
+                                 priority
+                              />
+                           </div>
+                           {persona}
+                        </CardTitle>
+                        <CardDescription>{description}</CardDescription>
+                     </CardHeader>
+                     <CardContent></CardContent>
+                  </Card>
+               </div>
+            );
+         })}
       </>
    );
 };
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,23 +1,30 @@
 import { Metadata } from 'next';
 import { ChatModelList } from './chat_model_list';
-import { GetChatModelListUseCase } from '@/be/application/chat/GetChatModelListUseCase';
-import { UnitOfWorkChatFactory } from '@/be/infrastructure/repository/uow/factory/UnitOfWorkChatFactory';
+import { env } from '@/lib/be/utils/env';
 export const metadata: Metadata = {
    title: 'AI With Chat List',
    description: 'AI Chatting Selection Model Page',
 };
 
 const ChatPage = async () => {
-   const useCase = new GetChatModelListUseCase(UnitOfWorkChatFactory.create());
-   const responseData = await useCase.execute();
-
-   console.log(responseData);
+   const fetchData = await fetch(`http://${env.nextPublicDomain}/api/chat/models`);
+   const data = await fetchData.json();
 
    return (
       <>
-         <div>Model Select</div>
-         <main className="flex-1 px-3 flex flex-col w-full items-start justify-start">
-            <ChatModelList data={responseData ?? []} />
+         <div className="w-full max-w-3xl px-2 mt-3">
+            <h1 className="text-3xl md:text-5xl font-bold pb-2 text-center break-words">
+               Chirpify, Start Learning with the Right <span style={{ color: '#0EF397' }}> AI</span>
+            </h1>
+
+            <h2
+               className="font-medium text-xl md:text-2xl mb-4 pb-4 text-center break-words"
+               style={{ color: '#7B7B7B' }}>
+               Choose a chat model to guide your journey in English.
+            </h2>
+         </div>
+         <main className="grid gap-4 mt-3 px-3 w-full grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <ChatModelList data={data ?? []} />
          </main>
       </>
    );
