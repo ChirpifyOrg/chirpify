2d5b1459a50809a0bd292b91c92a206183cc1c82
bm150 <jbm20dev@gmail.com>
2025-05-08 18:42:18 +0900

    feat : 디버깅을 위한 로그 추가

--- lib/be/prisma.ts
+++ lib/be/prisma.ts
@@ -4,6 +4,12 @@ const globalForPrisma = globalThis as unknown as {
    prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+   globalForPrisma.prisma ??
+   (process.env.NODE_ENV !== 'production'
+      ? new PrismaClient({
+           log: ['query', 'info', 'warn', 'error'],
+        })
+      : new PrismaClient());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
