c2a3b1a5813f000c99c322b9ccc68805c154248a
bm150 <jbm20dev@gmail.com>
2025-05-08 14:52:36 +0900

    chore : header tag 오류 수정

--- app/(auth)/components/header-auth.tsx
+++ app/(auth)/components/header-auth.tsx
@@ -9,7 +9,6 @@ export default async function AuthButton() {
    const {
       data: { user },
    } = await supabase.auth.getUser();
-
    // 로그인한 사용자가 아니면 null을 리턴
 
    if (user?.is_anonymous || !user) {
@@ -24,14 +23,18 @@ export default async function AuthButton() {
       return (
          <div className=" flex items-center gap-4 pl-2 justify-start xs:justify-end ">
             <div className=" w-1/12 xxs:w-1/4 xs:w-1/3 ">
-               <Button variant="outline" asChild className="flex-1 truncate text-left w-full">
-                  <Link href="/practice-english">Practice English Sentences</Link>
-               </Button>
+               <Link href="/practice-english">
+                  <Button variant="outline" asChild className="flex-1 truncate text-left w-full">
+                     Practice English Sentences
+                  </Button>
+               </Link>
             </div>
             <div className=" w-1/12 xxs:w-1/4 xs:w-1/3 ">
-               <Button variant="outline" className="flex-1 truncate text-left  w-full">
-                  <Link href="/chat">Chat With AI</Link>
-               </Button>
+               <Link href="/chat">
+                  <Button variant="outline" className="flex-1 truncate text-left  w-full">
+                     Chat With AI
+                  </Button>
+               </Link>
             </div>
             <div className="flex-shrink-0">
                <UserProfile />
