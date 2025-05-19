2053bff56ce93d0b601cd5335473e0113212b880
bm150 <jbm20dev@gmail.com>
2025-05-19 18:00:08 +0900

    fix : 잘못된 싱글톤 적용으로 인한 트랜잭션 적용 오류 수정

--- be/infrastructure/repository/uow/factory/UnitOfWorkChatFactory.ts
+++ be/infrastructure/repository/uow/factory/UnitOfWorkChatFactory.ts
@@ -5,9 +5,7 @@ import { IUnitOfWorkChat } from '@/be/domain/chat/IUnitOfWorkChat';
 import { UnitOfWorkChat } from '../../prisma/chat/UnitWorkChat';
 
 export class UnitOfWorkChatFactory {
-   private static instance: IUnitOfWorkChat;
    static create(): IUnitOfWorkChat {
-      if (!UnitOfWorkChatFactory.instance) UnitOfWorkChatFactory.instance = UnitOfWorkChat.create(prisma);
-      return UnitOfWorkChatFactory.instance; // 현재는 prisma로 지정
+      return UnitOfWorkChat.create(prisma); // 현재는 prisma로 지정
    }
 }
