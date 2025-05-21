7a1639fac974c56e8a2721096f82bd2963c3da23
bm150 <jbm20dev@gmail.com>
2025-05-21 16:24:52 +0900

    feat : AIReponseGeneratorFactory 의 확장성을 고려

--- be/application/AIResponseGeneratorFactory.ts
+++ be/application/AIResponseGeneratorFactory.ts
@@ -3,16 +3,21 @@ import { ApiResponseGenerator } from './ApiResponseGenerator';
 import { OpenAIChatService } from '../infrastructure/service/OpenAIChatService';
 
 // 향후 추가 모델 지원시 해당 기능 사용
-type AviliableAIModel = 'GPT';
+type AvailableAIModel = 'GPT' | 'Gemini' | 'Claude';
 
 export class AIReponseGeneratorFactory {
    private static instance: Record<string, ApiResponseGenerator<unknown, unknown>> = {};
 
-   static create(modelName: AviliableAIModel) {
+   static create(modelName: AvailableAIModel) {
       if (!AIReponseGeneratorFactory.instance[modelName]) {
          switch (modelName) {
             case 'GPT':
                AIReponseGeneratorFactory.instance[modelName] = new OpenAIChatService(env.openAPIKey);
+               break;
+            case 'Gemini':
+               throw new Error('Not implemented');
+            case 'Claude':
+               throw new Error('Not implemented');
          }
       }
       return AIReponseGeneratorFactory.instance[modelName];
