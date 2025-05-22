0aa9284aff2a06d9cb5eecd2e57929ec2eeaaf26
bm150 <jbm20dev@gmail.com>
2025-05-23 02:54:39 +0900

    fix : env test

--- lib/fe/superbase/signup.ts
+++ lib/fe/superbase/signup.ts
@@ -5,11 +5,12 @@ import { env } from '@/lib/be/utils/env';
 export async function signUpWithGoogle() {
    const supabase = await createClient();
    var protocol = env.isLocal() ? 'http' : 'https';
-
+   const redirectToUrl = `${protocol}://${process.env.NEXT_PUBLIC_DOMAIN ? process.env.NEXT_PUBLIC_DOMAIN : env.nextPublicDomain}/api/auth/callback/google`;
+   console.log('redirectTo:', redirectToUrl);
    const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-         redirectTo: `${protocol}://${process.env.VERCEL_URL ? process.env.VERCEL_URL : env.nextPublicDomain}/api/auth/callback/google`,
+         redirectTo: redirectToUrl,
       },
    });
 
