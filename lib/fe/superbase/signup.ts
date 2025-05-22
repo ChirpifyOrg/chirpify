4df8cc59fe74045198d9840d1128ef82ada617e1
bm150 <jbm20dev@gmail.com>
2025-05-23 02:39:35 +0900

    feat : 기존 적용되던 url 규칙 변경

--- lib/fe/superbase/signup.ts
+++ lib/fe/superbase/signup.ts
@@ -1,22 +1,21 @@
-"use client";
-import { createClient } from "@/lib/fe/superbase/client";
-import { env } from "@/lib/be/utils/env";
+'use client';
+import { createClient } from '@/lib/fe/superbase/client';
+import { env } from '@/lib/be/utils/env';
 
 export async function signUpWithGoogle() {
-  const supabase = await createClient();
-  var protocol = env.isLocal() ? "http" : "https";
+   const supabase = await createClient();
+   var protocol = env.isLocal() ? 'http' : 'https';
 
+   const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+         redirectTo: `${protocol}://${process.env.VERCEL_URL ? process.env.VERCEL_URL : env.nextPublicDomain}/api/auth/callback/google`,
+      },
+   });
 
-  const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: "google",
-    options: {
-      redirectTo: `${protocol}://${env.nextPublicDomain}/api/auth/callback/google`,
-    },
-  });
+   if (error) {
+      throw error;
+   }
 
-  if (error) {
-    throw error;
-  }
-
-  return data;
+   return data;
 }
