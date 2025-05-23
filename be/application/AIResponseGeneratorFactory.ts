7c853770c0b0cc22aae7d2b3bfbc3b98300a5ea0
bm150 <jbm20dev@gmail.com>
2025-05-23 14:04:08 +0900

    refactor : AIGenerator에 대한 타입 안전성을 높히기 위한 작업

--- be/application/AIResponseGeneratorFactory.ts
+++ be/application/AIResponseGeneratorFactory.ts
@@ -1,25 +1,28 @@
 import { env } from '@/lib/be/utils/env';
 import { ApiResponseGenerator } from './ApiResponseGenerator';
 import { OpenAIChatService } from '../infrastructure/service/OpenAIChatService';
+import { AvailableAIModelType } from '@/types/shared';
 
-// 향후 추가 모델 지원시 해당 기능 사용
-type AvailableAIModel = 'GPT' | 'Gemini' | 'Claude';
+type ModelMap = {
+   GPT: OpenAIChatService;
+   Gemini: null;
+   Claude: null;
+};
 
 export class AIReponseGeneratorFactory {
-   private static instance: Record<string, ApiResponseGenerator<unknown, unknown>> = {};
-
-   static create(modelName: AvailableAIModel) {
+   private static instance: Partial<{ [K in AvailableAIModelType]: ApiResponseGenerator<any, any> }> = {};
+   static create<M extends AvailableAIModelType>(modelName: M): ModelMap[M] {
       if (!AIReponseGeneratorFactory.instance[modelName]) {
          switch (modelName) {
             case 'GPT':
                AIReponseGeneratorFactory.instance[modelName] = new OpenAIChatService(env.openAPIKey);
                break;
-            case 'Gemini':
-               throw new Error('Not implemented');
             case 'Claude':
+               throw new Error('Not implemented');
+            case 'Gemini':
                throw new Error('Not implemented');
          }
       }
-      return AIReponseGeneratorFactory.instance[modelName];
+      return AIReponseGeneratorFactory.instance[modelName] as ModelMap[M];
    }
 }
