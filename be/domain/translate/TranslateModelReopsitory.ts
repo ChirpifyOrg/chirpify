fedbf28bd0cee81ff71d1ec25e93f6b2c87f095d
bm150 <jbm20dev@gmail.com>
2025-05-23 14:05:49 +0900

    feat : 영작 모델에 대한 정보를 받아올수 있는 repository interface method 추가

--- be/domain/translate/TranslateModelReopsitory.ts
+++ be/domain/translate/TranslateModelReopsitory.ts
@@ -1,1 +1,6 @@
-export interface TranslateModelReopsitory {}
+import { TranslateModelUseType } from '@/types/translate';
+import { TranslateModel } from './TranslateModel';
+
+export interface TranslateModelReopsitory {
+   findByUseTypeActive(useType: TranslateModelUseType): Promise<TranslateModel | null>;
+}
