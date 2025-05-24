93da22fe4a6b6cce5608ed4a22c73a0ba6497776
bm150 <jbm20dev@gmail.com>
2025-05-24 18:58:28 +0900

    feat : 문장 등록 및 모델 찾기 기능 추가

--- be/infrastructure/repository/prisma/translate/TranslateModelReopsitory.ts
+++ be/infrastructure/repository/prisma/translate/TranslateModelReopsitory.ts
@@ -1,4 +1,22 @@
 import { TranslateModelReopsitory } from '@/be/domain/translate/TranslateModelReopsitory';
 import { BasePrismaRepository } from '../BasePrismaRepository';
+import { TranslateModel } from '@/be/domain/translate/TranslateModel';
+import { TranslateModelUseType } from '@/types/translate';
 
-export class TranslateModelReopsitoryImpl extends BasePrismaRepository implements TranslateModelReopsitory {}
+export class TranslateModelReopsitoryImpl extends BasePrismaRepository implements TranslateModelReopsitory {
+   async findByUseTypeActive(useType: TranslateModelUseType): Promise<TranslateModel | null> {
+      try {
+         const response = await this.prisma.translate_model.findFirst({
+            where: {
+               use_type: useType,
+               is_active: true,
+            },
+         });
+         const entity = TranslateModel.fromPrisma(response);
+         return entity;
+      } catch (e) {
+         console.error(e);
+         throw e;
+      }
+   }
+}
